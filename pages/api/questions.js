9319fd02cefdf37c7443dd8dd660a78bcd5afffe
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-04-30 19:40:45 +0200

    widget selezione lingua con checkbox

--- pages/api/questions.js
+++ pages/api/questions.js
@@ -1,54 +1,97 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    data: {
-      sections: [
-        {
-          code: '1',
-          title: {
-            it: 'Lingue parlate e contesti comunicativi',
-          },
-        },
-        {
-          code: '2',
-          title: {
-            it: 'Competenza linguistica orale e scritta',
-          },
-        },
-      ],
-      subsections: [
-        {
-          code: '1.1.a',
-          title: {
-            it: 'Abitualmente...',
-          },
-        },
-        {
-          code: '1.1.b',
-          title: {
-            it: 'In famiglia abitualmente io parlo...',
-          }
-        },
-      ],
-      questions: [
-        {
-          code: "1.1.a.1",
+const data = text_to_json(`
+1. Lingue parlate e contesti comunicativi
+1.1.a. Abitualmente...
+* nella mia famiglia si parla...
+1.1.b. In famiglia abitualmente io parlo...
+* a mia mamma parlo in...
+* a mio papà parlo in...
+* ai miei fratelli/sorelle parlo in...
+* ai miei nonni parlo in...
+1.1.c. In famiglia abitualmente...
+* i miei genitori tra di loro parlano...
+* mia madre a me parla in...
+* mio padre a me parla in...
+* i miei fratelli/sorelle a me parlano in...
+* i miei nonni a me parlano in...
+1.2.a. Fuori casa abitualmente io parlo...
+* ai miei amici parlo in...
+* alle persone dei negoziparlo in...
+* agli adulti del miopaese/città/quartiere parlo in...
+* 1.2.b. Fuori casa abitualmente...
+* i miei amici a me parlano in...
+* le persone dei negozi ame parlano in...
+* gli adulti del miopaese/città/quartiere a me parlano in...
+* 1.3.a. A scuola abitualmente io parlo...
+* ai miei compagni di classe parlo in...
+* ai professori parlo in... (esclusi quelli di lingue)
+1.3.b. A scuola abitualmente...
+* i miei compagni di classea me parlano in...
+* i professori a me parlanoin... (esclusi quelli di lingue straniere)
+1.4. Abitualmente...
+* quali lingue usi quando telefoni agli amici?
+* quali lingue usi nelle mail, sui social e nelle chat?
+* quali lingue vengono usate nei programmi TV che guardi?
+* quali lingue vengono usate nei siti internet che visiti?
+* in quali lingue sono scritti i libri, le storie e i fumetti che leggi?
+1.5. A che età hai cominciato a parlare le lingue che conosci? (indica una sola fascia d'età per ciascuna voce)
+2. Competenza linguistica orale e scritta
+2.1. Italiano
+2.2. Friulano
+2.3. Sloveno
+2.4. Tedesco
+2.5. Inglese
+2.6. lingua 1
+2.7. lingua 2
+`)
+
+function text_to_json(text) {
+  let questions = []
+  let sections = []
+  let subsections = []
+
+  let last_code = null
+  let last_count = 0
+  text.split('\n')
+    .filter(line => line.length > 0)
+    .map(line => {
+      const i = line.indexOf(' ')
+      const code = line.substring(0, i)
+      const question = line.substring(i + 1)
+      if (code === '*') {
+        last_count ++
+        questions.push({
+          code: `${last_code}.${last_count}`,
           question: {
-            it: 'nella mia famiglia si parla...',
-          },
-        },
-        {
-          code: "1.1.b.1",
-          question: {
-            it: 'a mia mamma parlo in...',
+            it: question,
           }
-        },
-        {
-          code: "1.1.b.2",
-          question: {
-            it: 'a mia papà parlo in...',
-          }
-        },
-      ],
+        })
+      } else {
+        const split = code.split('.').filter(s => s.length > 0)
+        last_code = split.join('.')
+        last_count = 0
+        if (split.length === 1) {
+          sections.push({
+              code: last_code, 
+              title: {
+                it: question,
+              }})
+        } else {
+          subsections.push({
+              code: last_code,
+              title: {
+                it: question,
+              }})
+        }
+      }
+    })
+    return {
+      sections,
+      subsections,
+      questions,
     }
-  })
 }
+
+export default function handler(req, res) {
+  res.status(200).json({ data })
+}
+
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import useSWR from 'swr'
 import { useState } from 'react'
+import { Button } from 'react-bootstrap'
 
 import Header from '../components/Header'
 
@@ -16,31 +17,87 @@ function useQuestions () {
   }
 }
 
-const languages = {
+const languages_answer = {
   'it': {
-    'it': 'italiano',
+    'it': 'Italiano',
   },
   'fu': {
-    'it': 'friulano',
+    'it': 'Friulano (o varianti)',
   },
   'de': {
-    'it': 'tedesco',
+    'it': 'Tedesco',
+  },
+  'sl': {
+    'it': 'Sloveno',
   },
 }
 
-function LanguageChoice() {
-  return <select>
-    { Object.entries(languages).map(([code, data]) => <option key={code} value={code}>
-      { data.it }
-    </option>)}
-  </select>
+const Checkbox = ({ name, label, selected, setSelected }) => (
+  <div className="form-check">
+    <label>
+      <input
+        type="checkbox"
+        name={name || label}
+        checked={selected}
+        onChange={setSelected ? evt=>setSelected(evt.target.checked) : null}
+        className="form-check-input"
+      />
+      {label}
+    </label>
+  </div>
+)
+
+function LanguageCheckbox({ name, label, answer, setAnswer }) {
+  return <Checkbox
+    name={name}
+    label={label} 
+    selected={answer.includes(name)}
+    setSelected={(selected) => {
+      if (selected && !answer.includes(name)) {
+        setAnswer(a => [...a, name])
+      }
+      if (!selected && answer.includes(name)) {
+        setAnswer(a => a.filter(l => l !== name))
+      }
+    }}
+  />
 }
 
-function Question({ question, engine }) {
+function OtherLanguage({addLanguage}) {
+  const [other, setOther] = useState("")
+  return <>
+    Altra lingua: <input value={other} onChange={(evt)=>setOther(evt.target.value)}/>
+    {other && <Button onClick={()=>{
+      addLanguage(other)
+      setOther("")
+    }}>+</Button>}
+  </>
+}
+
+function LanguageAnswer({answer, setAnswer}) {
+  const other_answers = answer.filter(a => !Object.keys(languages_answer).includes(a))
+  return <>{[
+    ...Object.entries(languages_answer).map(([code, data]) => [code, data.it]),
+    ...other_answers.map(a => [a, a])
+    ].map(([code, language]) => 
+          <LanguageCheckbox 
+            key={code} 
+            name={code}
+            label={language}
+            answer={answer}
+            setAnswer={setAnswer}
+            />
+        )} 
+    <OtherLanguage addLanguage={(language) => setAnswer(a => [...a, language])}/> 
+  </>
+}
+
+function Question({ question }) {
+  const [answer, setAnswer] = useState([])
   return <div>
-    <p><b>{question.question.it}</b><br />
-    <LanguageChoice />
-    </p>
+    <b>{question.question.it}</b><br />
+    <LanguageAnswer answer={answer} setAnswer={setAnswer}/>
+    <br />
   </div>
 }
 
