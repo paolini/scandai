90b4a2c8805493281574611108ca79c7dc661209
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-05-27 09:26:16 +0200

    fix db handling

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,38 +1,18 @@
 import mongoose from 'mongoose'
-import { MongoClient } from 'mongodb'
 
-function connect(uri: string, options: any) {
-  return mongoose.connect(uri, options)
-//  const client = new MongoClient(uri, options)
-//  return client.connect()
+async function connect() {
+  const uri = process.env.MONGODB_URI
+  const options = {}
+
+  if (uri) {
+    console.log(`connecting to mongodb at ${uri}`)
+    await mongoose.connect(uri, options)
+    console.log("connected to mongodb")
+  } else {
+    console.log("no mongodb connection: set MONGODB_URI environment variable")
+  }
 }
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
-}  
+let connectPromise = connect()
 
-const uri = process.env.MONGODB_URI
-const options = {}
-
-let client
-let clientPromise: Promise<MongoClient>
-
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  let globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>
-  }
-
-  if (!globalWithMongo._mongoClientPromise) {
-    globalWithMongo._mongoClientPromise = connect(uri, options)
-  }
-  clientPromise = globalWithMongo._mongoClientPromise
-} else {
-  // In production mode, it's best to not use a global variable.
-  clientPromise = connect(uri, options)
-}
-
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
-export default clientPromise
+export default connectPromise
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 import Questions from './Questions'
 import Header from '../components/Header'
 
-import clientPromise from '../lib/mongodb'
+import connectedPromise from '../lib/mongodb'
 
 type ConnectionStatus = {
   isConnected: boolean
@@ -14,15 +14,7 @@ export const getServerSideProps: GetServerSideProps<
   ConnectionStatus
 > = async () => {
   try {
-    await clientPromise
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
+    await connectedPromise
 
     return {
       props: { isConnected: true },
