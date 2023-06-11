876f23ae23fc60ce6d3e0e3262e4ee6d97b65358
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-06-11 17:03:20 +0200

    policies: cookies, privacy, tos

---
+++ pages/cookies.tsx
@@ -0,0 +1,31 @@
+export default function Cookies({}) {
+    return <>
+      <h1>Politica sui cookie</h1>
+      <p>La presente politica sui cookie descrive come vengono utilizzati i cookie su questo sito web. Questa politica si applica a tutti i visitatori del sito.</p>
+    
+      <h2>Cosa sono i cookie?</h2>
+      <p>I cookie sono piccoli file di testo che vengono salvati sul tuo dispositivo quando visiti un sito web. Questi file contengono informazioni che possono essere lette dal sito stesso durante le tue visite successive. I cookie svolgono diverse funzioni, tra cui consentire al sito di riconoscerti come utente, ricordare le tue preferenze e fornirti una migliore esperienza di navigazione.</p>
+    
+      <h2>Tipi di cookie utilizzati</h2>
+      <ol>
+        <li><strong>Cookie strettamente necessari:</strong> Questi cookie sono essenziali per consentire il corretto funzionamento del sito web. Vengono utilizzati per autenticare gli utenti, gestire la sessione di navigazione e garantire la sicurezza del sito.</li>
+        <li><strong>Cookie di prestazione:</strong> Questi cookie raccolgono informazioni sull'utilizzo del sito web, come le pagine visitate e gli errori riscontrati. Questi dati vengono utilizzati per migliorare il funzionamento del sito e per raccogliere statistiche anonime sull'utilizzo.</li>
+        <li><strong>Cookie di funzionalità:</strong> Questi cookie consentono al sito web di ricordare le scelte che hai fatto (come la lingua o la regione) e di fornire funzionalità personalizzate. Possono anche essere utilizzati per fornire servizi richiesti, come la visualizzazione di un video o l'interazione con i social media.</li>
+        <li><strong>Cookie di targeting e pubblicitari:</strong> Questi cookie vengono utilizzati per mostrarti annunci pubblicitari pertinenti in base ai tuoi interessi. Possono essere forniti da terze parti e vengono utilizzati per tracciare la tua attività di navigazione su diversi siti web.</li>
+      </ol>
+    
+      <h2>Gestione dei cookie</h2>
+      <p>Puoi gestire le preferenze sui cookie utilizzando le impostazioni del tuo browser. Puoi scegliere di accettare o rifiutare i cookie e puoi anche cancellarli in qualsiasi momento. Tieni presente che la disabilitazione dei cookie potrebbe influire sulla funzionalità del sito web e su alcune caratteristiche potrebbero non essere disponibili.</p>
+    
+      <h2>Cookie di terze parti</h2>
+      <p>Il sito web potrebbe utilizzare cookie di terze parti per scopi di analisi o per mostrare annunci pubblicitari. Questi cookie sono soggetti alle politiche sulla privacy delle rispettive terze parti e non sono controllati dal sito web.</p>
+    
+      <h2>Aggiornamenti della politica</h2>
+      <p>La presente politica sui cookie potrebbe essere aggiornata di tanto in tanto per riflettere eventuali modifiche nel modo in cui utilizziamo i cookie o per rispettare le normative applicabili. Ti invitiamo a verificare periodicamente la politica per essere informato sulle eventuali modifiche.</p>
+    
+      <h2>Contattaci</h2>
+      <p>Se hai domande o dubbi riguardo alla presente politica sui cookie, ti invitiamo a contattarci tramite le informazioni di contatto fornite sul sito web.</p>
+    
+      <p>Ultimo aggiornamento: 11.6.2023</p>
+    </>
+}
