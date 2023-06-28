98392634d8cd495817f6ff1ad1f8aabb79d7c493
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-06-28 23:14:17 +0200

    getSessionUser

--- pages/api/admin.js
+++ pages/api/admin.js
@@ -1,27 +1,29 @@
 import { getServerSession } from 'next-auth' 
+import { getToken } from 'next-auth/jwt'
 
 import authOptions from "./auth/[...nextauth]"
 
 async function getSessionUser(req, res) {
+  const token = await getToken({req})
   const session = await getServerSession(req, res, authOptions)
-  return session
+  return token.dbUser
 }
 
 export default async (req, res) => {
-    const session = await getSessionUser(req, res, authOptions)
-    console.log(`* admin ${JSON.stringify(session)}`)
+    const user = await getSessionUser(req, res, authOptions)
+    console.log(`* admin ${JSON.stringify(user)}`)
 
 
-    if (session) {
+    if (user) {
         res.send({
           content:
             "This is protected content. You can access this content because you are signed in.",
-          session,
+          user,
         })
       } else {
         res.send({
           error: "You must be signed in to view the protected content on this page.",
-          session,
+          user,
         })
       }
     }
