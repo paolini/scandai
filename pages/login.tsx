3fedc111b78a7d39d8b693ff3d6f1ce5f7dc1d7a
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-09-13 19:05:12 +0200

    testing callbackUrl

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,31 +1,44 @@
 import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { ClientSafeProvider, getProviders, signIn, getCsrfToken } from "next-auth/react"
 import { getServerSession } from "next-auth/next"
-import authOptions from "./api/auth/[...nextauth]";
+import authOptions from "./api/auth/[...nextauth]"
+import { useSearchParams } from "next/navigation"
+
+import Error from '@/components/Error'
 
 export default function SignIn({ providers, csrfToken }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const searchParams = useSearchParams()
+  const callbackUrl = searchParams.get('callbackUrl') ?? undefined
+  const error = searchParams.get('error')
+
   return (
     <>
+      {error && <Error>{ error }</Error>}
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-            <ProviderSection provider={provider} csrfToken={csrfToken}/>
+            <ProviderSection provider={provider} csrfToken={csrfToken} callbackUrl={callbackUrl}/>
         </div>
       ))}
     </>
   )
 }
 
-function ProviderSection({ provider, csrfToken }: { 
+function ProviderSection({ provider, csrfToken, callbackUrl }: { 
     provider: ClientSafeProvider,
     csrfToken: string|undefined, 
+    callbackUrl: string|undefined,
 }) {
+    const querystring = callbackUrl === undefined ? '' : `?callbackUrl=${encodeURIComponent(callbackUrl)}`
+    // take the callback url from the query string
     switch(provider.type) {
         case 'oauth':
-            return <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+            return <>
+              <button onClick={() => signIn(provider.id,{callbackUrl})}>
+                Sign in with {provider.name}
+              </button>
+            </>
         case 'email':
-            return <form method="post" action="/api/auth/signin/email">
+            return <form method="post" action={`/api/auth/signin/email${querystring}`}>
                 <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
                 <label>
                     Email address
@@ -34,7 +47,7 @@ function ProviderSection({ provider, csrfToken }: {
                 <button type="submit">Sign in with Email</button>
             </form>
         case 'credentials':
-            return <form method="post" action="/api/auth/callback/credentials">
+            return <form method="post" action={`/api/auth/callback/credentials${querystring}`}>
                 <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
                 <label>
                 Username
