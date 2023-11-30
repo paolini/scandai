1115ea4ae7122f1a2f4a99e6ba97fa1b68005e2f
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-11-30 08:09:18 +0100

    fix grafico conta lingue closes #44

--- pages/report.tsx
+++ pages/report.tsx
@@ -394,6 +394,7 @@ function ReportChart({ question, item, t } : {
                 case 'count':
                     return <Item small={true} title={item_title || 'yyy'}>
                             <GraphChooseLanguageQuestionCounts item={item} stat={question} />
+                            <TableChooseLanguageQuestionCounts item={item} stat={question} />
                     </Item>
             }
         case 'map-language-to-competence': return <Item>
@@ -580,7 +581,7 @@ function GraphChooseLanguageQuestionCounts({item,stat}: {
                 },
                 datalabels: {
                     anchor: 'center',
-                    formatter: (value,context) => `${Math.round(value*100/total)}% ${nLanguages(data[context.dataIndex][1])}`,
+                    formatter: (value,context) => value>0.01*total ? `${Math.round(value*100/total)}% ${nLanguages(data[context.dataIndex][1])}`:null,
                 },
 //                title: {
 //                    ...CHART_TITLE,
@@ -594,6 +595,74 @@ function GraphChooseLanguageQuestionCounts({item,stat}: {
                 {
                 data: data.map(([v,i])=>v),
                // backgroundColor: 'orange',
+                },
+            ],
+        }} 
+    />
+}
+
+function TableChooseLanguageQuestionCounts({item,stat}: {
+    item: IReportElement,
+    stat: IChooseLanguageQuestionStat}) {
+    const _ = useTrans()
+    //    const item_title = item.title && trans(item.title, _.locale)
+
+    function nLanguages(count: number) {
+        if (count===0) return _("Nessuna lingua")
+        if (count===1) return _("Una lingua")
+        return _("% lingue", count)
+    }
+    assert(stat.counts)
+    assert(item.element === 'chart')
+    const data = stat.counts
+        .map((v,i)=>[v,i])
+        .filter(([v,i])=>v>0)
+
+    const total = data.reduce((acc, [v,i])=>acc+v, 0)
+
+    return <Table>
+        <thead>
+            <tr>
+                <td></td>
+                { data.map(([v,i])=><th key={i}>{nLanguages(i)}</th>) }
+            </tr>
+        </thead>    
+        <tbody>
+            <tr>
+                <th>{_("conteggio")}</th>
+                { data.map(([v,i])=><td key={i}>{v}</td>) }
+            </tr>
+            <tr>
+                <th>{_("percentuale")}</th>
+                { data.map(([v,i])=><td key={i}>{Math.round(v*100/total)}%</td>) }
+            </tr>
+        </tbody>        
+    </Table>
+
+    return <Doughnut
+        options={{
+            responsive: true,
+            plugins: {
+                legend: {
+                    display: false,
+                    position: 'top' as const,
+                },
+                datalabels: {
+                    anchor: 'center',
+                    formatter: (value,context) => value>0.01*total ? `${Math.round(value*100/total)}% ${nLanguages(data[context.dataIndex][1])}`:null,
+                },
+    //                title: {
+    //                    ...CHART_TITLE,
+    //                    text: item_title || stat.question.question.it,
+    //                },
+            },
+        }} 
+        data={{
+            labels: data.map(([v,i])=>nLanguages(i)),
+            datasets: [
+                {
+                data: data.map(([v,i])=>v),
+            // backgroundColor: 'orange',
                 },
             ],
         }} 
