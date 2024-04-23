1dcb6725d88bb1a707a1fa92db669671be7a5f06
Emanuele Paolini <emanuele.paolini@gmail.com>
2024-04-23 12:01:07 +0200

    bugfix creating new school

--- pages/api/schools/index.ts
+++ pages/api/schools/index.ts
@@ -8,7 +8,6 @@ import getSessionUser from '@/lib/getSessionUser'
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse) {
-        // await delay(5000)
         const user = await getSessionUser(req)
         if (!user) {
             return res.status(401).json({error: 'not authenticated'})
@@ -34,8 +33,8 @@ export default async function handler(
             return res.status(403).json({error: 'not authorized'})
         }
         if (req.method === 'POST') {
-            const {name, city } = JSON.parse(req.body)
-            const newSchool = new School({name, city })
+            const {name, city, city_fu } = JSON.parse(req.body)
+            const newSchool = new School({name, city, city_fu})
             console.log(`creating new School: ${newSchool}`)
             const out = await newSchool.save()
             return res.json({data: out})
